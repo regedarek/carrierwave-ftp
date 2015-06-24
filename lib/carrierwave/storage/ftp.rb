cad68b8c27e8179d77cab819c79032fba4224428
Darek Finster <darek.finster@gmail.com>
2015-06-24 13:35:50 +0200

    Add ssl to ftp

--- lib/carrierwave/storage/ftp.rb
+++ lib/carrierwave/storage/ftp.rb
@@ -83,11 +83,13 @@ module CarrierWave
 
         def file
           require 'net/http'
-          url = URI.parse(self.url)
-          req = Net::HTTP::Get.new(url.path)
-          Net::HTTP.start(url.host, url.port) do |http|
-            http.request(req)
-          end
+          uri = URI.parse(self.url)
+          http = Net::HTTP.new(uri.host, uri.port)
+          http.use_ssl = true
+          http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+          request = Net::HTTP::Get.new(uri.request_uri)
+          response = http.request(request)
+          response
         rescue
         end
 
